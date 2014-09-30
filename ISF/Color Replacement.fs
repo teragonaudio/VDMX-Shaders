cfa0551828d24ba915accac578de4b2565b63e5a
Nik Reiman <nik.reiman@gmail.com>
2014-09-30 18:32:32 +0200

    Remove noise from Color Replacement

--- ISF/Color Replacement.fs
+++ ISF/Color Replacement.fs
@@ -33,14 +33,6 @@
       ]
     },
     {
-      "NAME": "noise",
-      "LABEL": "Noise amount",
-      "TYPE": "float",
-      "MIN": 0.0,
-      "MAX": 1.0,
-      "DEFAULT": 0.0
-    },
-    {
       "NAME": "tolerance",
       "LABEL": "Color detection tolerance",
       "TYPE": "float",
@@ -60,23 +52,15 @@ void main(void) {
   vec4 inPixel = IMG_THIS_PIXEL(inputImage);
   vec4 outPixel = inPixel;
   
-  if(inPixel.r >= (targetColor.r - tolerance) &&
-     inPixel.r <= (targetColor.r + tolerance)) {
-    if(inPixel.g >= (targetColor.g - tolerance) &&
-       inPixel.g <= (targetColor.g + tolerance)) {
-      if(inPixel.b >= (targetColor.b - tolerance) &&
-         inPixel.b <= (targetColor.b + tolerance)) {
-        float noiseAmountR = 0.0;
-        float noiseAmountG = 0.0;
-        float noiseAmountB = 0.0;
-        if(noise > 0.0) {
-          noiseAmountR = noise * rand(vec2(TIME, gl_FragColor.r));
-          noiseAmountG = noise * rand(vec2(TIME, gl_FragColor.g));
-          noiseAmountB = noise * rand(vec2(TIME, gl_FragColor.b));
-        }
-        outPixel.r = replacementColor.r + noiseAmountR;
-        outPixel.g = replacementColor.g + noiseAmountG;
-        outPixel.b = replacementColor.b + noiseAmountB;
+  if (inPixel.r >= (targetColor.r - tolerance) &&
+      inPixel.r <= (targetColor.r + tolerance)) {
+    if (inPixel.g >= (targetColor.g - tolerance) &&
+        inPixel.g <= (targetColor.g + tolerance)) {
+      if (inPixel.b >= (targetColor.b - tolerance) &&
+          inPixel.b <= (targetColor.b + tolerance)) {
+        outPixel.r = replacementColor.r;
+        outPixel.g = replacementColor.g;
+        outPixel.b = replacementColor.b;
       }
     }
   }
